5854f92ecf5237a18956850f535417ace688b16e
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-07-05 15:31:44 +1000

    add functionality to save word pairs as fav

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,51 +12,5 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
             primarySwatch: Colors.purple, primaryColor: Colors.purple[900]),
         home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _randomWordPairs = <WordPair>[];
-
-  Widget _buildList() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, item) {
-          if (item.isOdd) return const Divider();
-
-          final index = item ~/ 2;
-
-          if (index >= _randomWordPairs.length) {
-            _randomWordPairs.addAll(generateWordPairs().take(10));
-          }
-
-          return _buildRow(_randomWordPairs[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-        title: Text(
-      pair.asPascalCase,
-      style: const TextStyle(fontSize: 18),
-    ));
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'WordPair Generator',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.purple[900],
-      ),
-      body: _buildList(),
-    );
   }
 }
